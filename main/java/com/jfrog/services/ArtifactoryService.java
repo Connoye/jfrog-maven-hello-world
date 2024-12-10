dbb34dc807f12336c6b6ee1c567fc86915cbae47
Connoye <95093333+Connoye@users.noreply.github.com>
2024-12-10 11:48:49 -0500

    maven installation

---
+++ main/java/com/jfrog/services/ArtifactoryService.java
@@ -0,0 +1,11 @@
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.Map;
+
+@Service
+public class ArtifactoryService {
+}
